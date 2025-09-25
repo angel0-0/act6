bc01a65f3e0d9a291c0f7f881ad6c6951fdefc0d
angel <angel.herr.808@gmail.com>
2025-09-24 20:31:28 -0600

    app terminada

--- app/src/main/java/com/angel/act6/Scene2Activity.java
+++ app/src/main/java/com/angel/act6/Scene2Activity.java
@@ -7,18 +7,18 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Scene2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scene2);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Scene2Activity.class);
+                Intent intent = new Intent(Scene2Activity.this, MainActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.explode_in, R.anim.explode_out);
             }
